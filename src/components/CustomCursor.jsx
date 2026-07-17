702aebb77c608713ec4f369263e4e74678e71c7e
sarvesh0506 <sarveshkumarsps206@gmail.com>
2026-07-17 23:45:43 +0530

    UI: Enlarge custom cursor pointer and increase scroll reveal scale & slide distance

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -30,7 +30,7 @@ export default function CustomCursor() {
 
   return (
     <motion.div
-      className="fixed top-0 left-0 w-3 h-3 rounded-full bg-coca-red pointer-events-none -translate-x-1/2 -translate-y-1/2 z-[9999] shadow-[0_0_10px_rgba(228,30,38,0.8)]"
+      className="fixed top-0 left-0 w-5 h-5 rounded-full bg-coca-red pointer-events-none -translate-x-1/2 -translate-y-1/2 z-[9999] shadow-[0_0_15px_rgba(228,30,38,0.9)]"
       style={{
         left: cursorX,
         top: cursorY,
